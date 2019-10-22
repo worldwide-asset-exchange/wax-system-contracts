ec27396f9ba48f6f9a95b3c2ba1496d0010b4ad9
Javier Albiero <javier.albiero@opskins.com>
2019-10-22 12:35:55 -0300

    [KEW-43] Fix standby weight

--- contracts/eosio.system/src/voting.cpp
+++ contracts/eosio.system/src/voting.cpp
@@ -110,8 +110,8 @@ namespace eosiosystem {
       auto constexpr one_percent_weight = total_weight * 0.01;
       auto constexpr num_standbys = 36;
 
-      // multiplied by 21 because we are effectively making 21 separate random selection to insert a standby in this round
-      auto constexpr standby_weight = 21 * one_percent_weight / num_standbys; 
+      // multiplied by 10 (120/12, 12 blocks in 120 seconds)
+      auto constexpr standby_weight = 10 * one_percent_weight / num_standbys; 
 
       auto const selected_weight = to_int(previous_block_hash) % total_weight;
       auto const standby_index = selected_weight / standby_weight;
