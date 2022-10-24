b255584b55e0a9a3148ec28247426ee918f8e6f0
Henrik Hautakoski <henrik@eossweden.org>
2022-10-24 14:31:14 +0200

    Remove powerup

--- contracts/eosio.system/src/powerup.results.cpp
+++
@@ -1,5 +0,0 @@
-#include <eosio.system/powerup.results.hpp>
-
-void powup_results::powupresult( const asset& fee, const int64_t powup_net_weight, const int64_t powup_cpu_weight ) { }
-
-extern "C" void apply( uint64_t, uint64_t, uint64_t ) { }
