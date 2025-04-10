02e7d0b0a2c9342808892acd1df52377c933142b
Aaron Caswell <aaron@captureplay.com>
2025-04-09 17:38:37 -0700

    make calculations more readable

--- contracts/eosio.system/src/producer_pay.cpp
+++ contracts/eosio.system/src/producer_pay.cpp
@@ -88,8 +88,10 @@ namespace eosiosystem {
          auto to_voters        = 2 * to_per_block_pay;
          auto to_savings       = distribute_tokens - (to_voters + to_per_block_pay);
 
-         auto to_producers_pay = to_per_block_pay * 21 * STANDBY_PAY_RATIO_DENOMINATOR / ( _gstate4.standby_pay_ratio_numerator * _gstate4.num_standby_slots + 21 * STANDBY_PAY_RATIO_DENOMINATOR);
-         auto to_standby_pay   = to_per_block_pay - to_producers_pay;
+         auto total_block_pay = to_per_block_pay;
+         auto total_weight = (21ULL * PAY_SPLIT_SCALE) + (_gstate4.standby_slot_weight * _gstate4.num_standby_slots);
+         auto to_producers_pay = (total_block_pay * 21ULL * PAY_SPLIT_SCALE) / total_weight;
+         auto to_standby_pay   = total_block_pay - to_producers_pay;
          {
             if( issue_tokens > 0 ){
                token::issue_action issue_act{ token_account, { {get_self(), active_permission} } };
