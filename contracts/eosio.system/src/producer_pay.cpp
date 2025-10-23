98e88bb018f5675f9ecd669bb71ec11a69cd755b
Long Vu <hlongvu@gmail.com>
2025-10-23 13:27:29 +0700

    fix producer size

--- contracts/eosio.system/src/producer_pay.cpp
+++ contracts/eosio.system/src/producer_pay.cpp
@@ -89,14 +89,16 @@ namespace eosiosystem {
          auto to_voters        = 2 * to_per_block_pay;
          auto to_savings       = distribute_tokens - (to_voters + to_per_block_pay);
 
-         auto total_block_pay = to_per_block_pay;
-         auto total_weight = (producer_size * PAY_SPLIT_SCALE) + (_gstate4.standby_slot_weight * _gstate4.num_standby_slots);
-         auto to_producers_pay = (total_block_pay * producer_size * PAY_SPLIT_SCALE) / total_weight;
-         auto to_standby_pay   = total_block_pay - to_producers_pay;
          auto producer_size    =  _gstate.last_producer_schedule_size;
          if (producer_size == 0) {
             producer_size = 21;
          }
+         
+         auto total_block_pay = to_per_block_pay;
+         auto total_weight = (producer_size * PAY_SPLIT_SCALE) + (_gstate4.standby_slot_weight * _gstate4.num_standby_slots);
+         auto to_producers_pay = (total_block_pay * producer_size * PAY_SPLIT_SCALE) / total_weight;
+         auto to_standby_pay   = total_block_pay - to_producers_pay;
+         
          // auto to_producers_pay = to_per_block_pay * producer_size * STANDBY_PAY_RATIO_DENOMINATOR / ( _gstate4.standby_pay_ratio_numerator * _gstate4.num_standby_slots + producer_size * STANDBY_PAY_RATIO_DENOMINATOR);
          // auto to_standby_pay   = to_per_block_pay - to_producers_pay;
          {
